fe1ee1a4f7089f58ecf82a729bc1d7be97eed8a1
webarata3 <webarata3@gmail.com>
2019-05-25 16:08:32 +0900

    リンクを別ウィンドウに変更

--- elm/src/View/Footer.elm
+++ elm/src/View/Footer.elm
@@ -9,24 +9,40 @@ viewFooter =
     footer []
         [ div [ class "copyright" ]
             [ text "©2019 "
-            , a [ href "https://webarata3.dev" ] [ text "Shinichi ARATA（webarata3）" ]
+            , a
+                [ href "https://webarata3.dev"
+                , target "_blank"
+                ]
+                [ text "Shinichi ARATA（webarata3）" ]
             ]
         , div [ class "sns" ]
             [ ul []
                 [ li []
-                    [ a [ href "https://twitter.com/webarata3" ]
+                    [ a
+                        [ href "https://twitter.com/webarata3"
+                        , target "_blank"
+                        ]
                         [ span [ class "fab fa-twitter" ] [] ]
                     ]
                 , li []
-                    [ a [ href "https://facebook.com/arata.shinichi" ]
+                    [ a
+                        [ href "https://facebook.com/arata.shinichi"
+                        , target "_blank"
+                        ]
                         [ span [ class "fab fa-facebook" ] [] ]
                     ]
                 , li []
-                    [ a [ href "https://github.com/webarata3" ]
+                    [ a
+                        [ href "https://github.com/webarata3"
+                        , target "_blank"
+                        ]
                         [ span [ class "fab fa-github" ] [] ]
                     ]
                 , li []
-                    [ a [ href "https://ja.stackoverflow.com/users/2214/webarata3?tab=profile" ]
+                    [ a
+                        [ href "https://ja.stackoverflow.com/users/2214/webarata3?tab=profile"
+                        , target "_blank"
+                        ]
                         [ span [ class "fab fa-stack-overflow" ] [] ]
                     ]
                 ]
