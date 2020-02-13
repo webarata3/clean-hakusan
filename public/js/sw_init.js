22b805a326e78735b1e1b38a0bd6911276279578
webarata3 <webarata3@gmail.com>
2020-02-13 20:08:14 +0900

    console.logを削除

--- public/js/sw_init.js
+++ public/js/sw_init.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker.register('/service-worker.js').then(() => {
-      console.log('登録成功');
+      // console.log('登録成功');
     }, () => {
-      console.log('登録失敗');
+      // console.log('登録失敗');
     });
   });
 }
