3decf56d4c0d5e68fec76edeb5e5823d992725bb
webarata3 <webarata3@gmail.com>
2021-05-29 15:03:52 +0900

    iOSの100vh調整

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,52 +1,52 @@
 // キャッシュファイルの指定
-const CACHE_NAME = "cache-20200214-01";
-const cacheWhitelist = ["cache-20200214-01"];
+const CACHE_NAME = 'cache-20210529-01';
+const cacheWhitelist = ['cache-20210529-01'];
 const urlsToCache = [
-  "/",
-  "/css/font.css",
-  "/css/main.css",
-  "/css/reset.css",
-  "/font/FiraSans-Light.ttf",
-  "/font/FiraSans-Regular.ttf",
-  "/font/FiraSans.woff2",
-  "/font/Inconsolata-Bold.ttf",
-  "/font/Inconsolata-Regular.ttf",
-  "/font/Inconsolata.woff2",
-  "/js/sw_init.js",
-  "/image/icons/apple-touch-icon-152x152.png",
-  "/image/icons/favicon-16x16.png",
-  "/image/icons/favicon-32x32.png",
-  "/image/icons/safari-pinned-tab.svg",
-  "/image/icons/msapplication-icon-144x144.png",
-  "/js/init.js",
-  "/js/main.js",
-  "/js/util.js",
-  "/webfont/fa-brands-400.eot",
-  "/webfont/fa-brands-400.svg",
-  "/webfont/fa-brands-400.ttf",
-  "/webfont/fa-brands-400.woff",
-  "/webfont/fa-brands-400.woff2",
-  "/webfont/fa-regular-400.eot",
-  "/webfont/fa-regular-400.svg",
-  "/webfont/fa-regular-400.ttf",
-  "/webfont/fa-regular-400.woff",
-  "/webfont/fa-regular-400.woff2",
-  "/webfont/fa-solid-900.eot",
-  "/webfont/fa-solid-900.svg",
-  "/webfont/fa-solid-900.ttf",
-  "/webfont/fa-solid-900.woff",
-  "/webfont/fa-solid-900.woff2",
-  "/index.html"
+  '/',
+  '/css/font.css',
+  '/css/main.css',
+  '/css/reset.css',
+  '/font/FiraSans-Light.ttf',
+  '/font/FiraSans-Regular.ttf',
+  '/font/FiraSans.woff2',
+  '/font/Inconsolata-Bold.ttf',
+  '/font/Inconsolata-Regular.ttf',
+  '/font/Inconsolata.woff2',
+  '/js/sw_init.js',
+  '/image/icons/apple-touch-icon-152x152.png',
+  '/image/icons/favicon-16x16.png',
+  '/image/icons/favicon-32x32.png',
+  '/image/icons/safari-pinned-tab.svg',
+  '/image/icons/msapplication-icon-144x144.png',
+  '/js/init.js',
+  '/js/main.js',
+  '/js/util.js',
+  '/webfont/fa-brands-400.eot',
+  '/webfont/fa-brands-400.svg',
+  '/webfont/fa-brands-400.ttf',
+  '/webfont/fa-brands-400.woff',
+  '/webfont/fa-brands-400.woff2',
+  '/webfont/fa-regular-400.eot',
+  '/webfont/fa-regular-400.svg',
+  '/webfont/fa-regular-400.ttf',
+  '/webfont/fa-regular-400.woff',
+  '/webfont/fa-regular-400.woff2',
+  '/webfont/fa-solid-900.eot',
+  '/webfont/fa-solid-900.svg',
+  '/webfont/fa-solid-900.ttf',
+  '/webfont/fa-solid-900.woff',
+  '/webfont/fa-solid-900.woff2',
+  '/index.html'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       // Cache hit - return response
@@ -58,7 +58,7 @@ self.addEventListener("fetch", event => {
   );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames =>
       Promise.all(
