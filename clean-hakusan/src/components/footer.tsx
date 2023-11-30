a41949a374184320c32f43453cfe67c381949e28
webarata3 <webarata3@gmail.com>
2023-11-30 12:30:50 +0900

    Footerを上下中央寄せ

--- clean-hakusan/src/components/footer.tsx
+++ clean-hakusan/src/components/footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className="flex justify-between m-1">
+    <footer className="flex justify-between items-center m-1">
       <small className="text-base">©2023 webarata3（ARATA Shinichi）</small>
       <a href="https://twitter.com/webarata3" target="_blank" className="w-4">
         <Image src="/image/twitter.svg" width={16} height={16} alt="" />
